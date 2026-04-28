f6a6164a3e12f349e68920b669420cc6650861f5
Knartzpirat <lasse.schnitker@gmx.de>
2026-04-28 03:08:35 +0200

    feat: update Bambu Companion Cards version to 1.3.5

--- custom_components/bambu_companion/const.py
+++ custom_components/bambu_companion/const.py
@@ -8,7 +8,7 @@ URL_BASE = "/bambu_companion_static"
 BAMBU_COMPANION_CARDS = [
     {
         "filename": "bambu-companion-cards.js",
-        "version": "1.3.4",
+        "version": "1.3.5",
         "name": "Bambu Companion Cards",
     }
 ]
